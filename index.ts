b8b2cf15b08c5fc21afed2032a1cb622dc62583d
Mehdyafatima <misscode000@gmail.com>
2024-06-20 09:41:05 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colourfull wellcome message
+console.log(chalk.bold.bgCyanBright("\n \t\t Code With Mehdya Word-Counter"));
+console.log("=".repeat(60));
+//prompt user to enter a sentence
+let answers = await inquirer.prompt([{
+    name: "sentence",
+    type: "input",
+    message: "Enter a sentence",
+}]);
+
+//trimming the sentence  and spliting it into words based on "spaces"
+
+let words = answers.sentence.trim().split(" ");
+
+//Analysis of user input sentence
+
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
+
